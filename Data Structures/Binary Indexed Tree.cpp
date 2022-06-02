57eb8bcd3cfde0571a19270a16be3d630646b5db
joacarrilloco <30640717+joacarrilloco@users.noreply.github.com>
2022-06-01 20:09:23 -0500

    Update Binary Indexed Tree.cpp

--- Data Structures/Binary Indexed Tree.cpp
+++ Data Structures/Binary Indexed Tree.cpp
@@ -2,46 +2,46 @@ const int LG = 20;
 
 struct BIT
 {
-	vector< long long > bit;
+  vector< long long > bit;
 
-	BIT( int n )
-	{
-		bit.resize( n + 1 );
-	}
+  BIT( int n )
+  {
+    bit.resize( n + 1 );
+  }
 
-	void add( int x, int del )
-	{
-		for( ++x; x < (int) bit.size(); x += x & -x )
-			bit[x] += del;
-	}
+  void add( int x, int del )
+  {
+    for( ++x; x < (int) bit.size(); x += x & -x )
+      bit[x] += del;
+  }
 
-	long long query( int x )
-	{
-		long long ans = 0;
-		for( ++x; x; x -= x & -x )
-			ans += bit[x];
+  long long query( int x )
+  {
+    long long ans = 0;
+    for( ++x; x; x -= x & -x )
+      ans += bit[x];
 
-		return ans;
-	}
+    return ans;
+  }
 
-	long long query_range( int l, int r )
-	{
-		return query( r ) - query( l - 1 );
-	}
+  long long query_range( int l, int r )
+  {
+    return query( r ) - query( l - 1 );
+  }
 
-	int query_last_LE( long long lb )
-	{
-		long long sum = 0;
-		int x = 0;
-		for( int l = LG; l >= 0; --l )
-		{
-			int y = x + ( 1 << l );
-			if( y < (int) bit.size() && sum + bit[y] <= lb )
-			{
-				x = y;
-				sum += bit[x];
-			}
-		}
-		return --x;
-	}
+  int query_last_LE( long long lb )
+  {
+    long long sum = 0;
+    int x = 0;
+    for( int l = LG; l >= 0; --l )
+    {
+      int y = x + ( 1 << l );
+      if( y < (int) bit.size() && sum + bit[y] <= lb )
+      {
+        x = y;
+        sum += bit[x];
+      }
+    }
+    return --x;
+  }
 };
